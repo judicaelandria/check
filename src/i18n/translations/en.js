3531394bc89efd67db0e2993601131145a97b622
Juan Colombo <42392347+ColomboJuan@users.noreply.github.com>
2020-07-08 02:40:32 -0300

    Little fixes i18n (#16)
    Add translation for "Delete all Task"

--- src/i18n/translations/en.js
+++ src/i18n/translations/en.js
@@ -2,6 +2,7 @@ const en = {
   "Home.taskName": "Task Name",
   "Home.addNewTask": "Add new Task",
   "Home.noTask": "you have no task yet. Add a new one!",
+  "Nav.deleteAllTask": "Delete all task",
 };
 
 export default en;
--- src/i18n/translations/es.js
+++ src/i18n/translations/es.js
@@ -2,6 +2,7 @@ const es = {
   "Home.taskName": "Nombre de la tarea",
   "Home.addNewTask": "Agregar Tarea",
   "Home.noTask": "No tienes niguna tarea aún. Agrega una nueva",
+  "Nav.deleteAllTask": "Eliminar todas las tareas",
 };
 
 export default es;
